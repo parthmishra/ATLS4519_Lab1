72c1666745cb1a370a5aa039990a1c8140683729
Parth Mishra <parthmishra@engr2gw-0-10-dhcp.int.colorado.edu>
2016-02-04 14:43:54 -0700

    Adding Lab 2

---
+++ Lab2/Lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Parth Mishra on 2/4/16.
+//  Copyright © 2016 Parth Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
