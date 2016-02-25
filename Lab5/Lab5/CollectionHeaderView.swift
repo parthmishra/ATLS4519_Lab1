33b27bfafc9b4ef45144e9d27cf29f046825f697
Parth Mishra <parthmishra@Parths-iMac.local>
2016-02-24 22:03:18 -0700

    added Lab 5

---
+++ Lab5/Lab5/CollectionHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionHeaderView.swift
+//  Lab5
+//
+//  Created by Parth Mishra on 2/13/16.
+//  Copyright © 2016 Parth Mishra. All rights reserved.
+//
+
+import UIKit
+
+class CollectionSupplementaryView: UICollectionReusableView {
+        
+    @IBOutlet weak var headerLabel: UILabel!
+    
+
+}
---
+++ Lab5/Lab5/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Lab5
+//
+//  Created by Parth Mishra on 2/11/16.
+//  Copyright © 2016 Parth Mishra. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+}
