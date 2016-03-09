20992922397c30228d19a50fff21d6037430956c
Parth Mishra <parthmishra@Parths-iMac.local>
2016-03-09 16:01:49 -0700

    Project 1

---
+++ Project1/Project1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Project1
+//
+//  Created by Parth Mishra on 3/9/16.
+//  Copyright © 2016 Parth Mishra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
